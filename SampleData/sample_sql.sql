dfddd285f0f40c289c746033813968395da367f6
Sertainty-Admin <administrator@sertainty.com>
2025-03-25 13:48:26 -0500

    Add files via upload

---
+++ SampleData/sample_sql.sql
@@ -0,0 +1,14 @@
+-- \copyright Sertainty Corporation, 2019.  All Rights Reserved.
+--
+-- \file     sample_sql.sql
+--
+-- \brief  Sertainty sample SQL script
+--
+-- \author   Greg Smith
+-- \date     12/13/2019
+ 
+print "Adding data (script)";
+
+insert into t1 (c1,c2) values ('Greg',7);
+insert into t1 (c1,c2) values ('Rose',38);
+insert into t1 (c1,c2) values ('George',12);
